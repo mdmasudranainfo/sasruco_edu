91f18161d430d7210c0a66ff8a31336b98646519
mdmasudranainfo <mdmasude7@gmail.com>
2023-05-22 18:00:55 +0600

    adf

--- main.js
+++ main.js
@@ -47,10 +47,32 @@ $(window).scroll(function () {
   $("nav").toggleClass("addBg", $(this).scrollTop() > 50);
 });
 
-document.getElementById("accordion").addEventListener("click", () => {
-  const OffOn = document.getElementById("OffOn");
-  OffOn.classList.toggle("hidden");
-  // OffOn.classList.toggle("opacity-0");
-});
+// document.getElementById("accordion").addEventListener("click", () => {
+//   const OffOn = document.getElementById("OffOn");
+//   OffOn.classList.toggle("hidden");
+//   // OffOn.classList.toggle("opacity-0");
+// });
 
 //
+
+// contact page js accordtion with
+// accordion 1
+document.getElementById("Ac1").addEventListener("click", function () {
+  const element = document.getElementById("Acp1");
+  element.classList.toggle("hidden");
+});
+// accordion 2
+document.getElementById("Ac2").addEventListener("click", function () {
+  const element = document.getElementById("Acp2");
+  element.classList.toggle("hidden");
+});
+// accordion 3
+document.getElementById("Ac3").addEventListener("click", function () {
+  const element = document.getElementById("Acp3");
+  element.classList.toggle("hidden");
+});
+// accordion 4
+document.getElementById("Ac4").addEventListener("click", function () {
+  const element = document.getElementById("Acp4");
+  element.classList.toggle("hidden");
+});
